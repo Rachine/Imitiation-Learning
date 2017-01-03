c29ae244fad12a54b64907aebf59b401241f2b04
rachine <riadrachid3@gmail.com>
2017-01-03 23:56:18 +0100

    process ocr dataset

--- dagger.py
+++ dagger.py
@@ -18,16 +18,32 @@ class DAgger(object):
     def __init__(self,ocr_path = 'letter.data'):
         self.ocr_path = os.path.join("Dataset/", str(ocr_path))
         self.words = []
+        self.words_fold = []
+        self.sequences = []
+    
+    def process_ocr(self):
+        """Process ocr dataset"""
         letters = []
+        sequence = []
         f = open(self.ocr_path, 'r')
         for line in f.readlines():
             line = line.split('\t')
             if int(line[2]) == -1:
                 line.remove('\n')
-                letters.append(map(lambda letter: letter.encode('utf-8'), line))
+                sequence.append(line[1])
+                image = np.array(map(lambda letter: int(letter), line[6:])).reshape((16,8))
+                letters.append(image)
                 self.words.append(letters)
+                self.sequences.append(sequence)
+                self.words_fold.append(line[5])
                 letters = []
+                sequence = []
                 continue
             line.remove('\n')
-            letters.append(map(lambda letter: letter.encode('utf-8'), line))
+            line = map(lambda letter: letter.encode('utf-8'), line)
+            sequence.append(line[1])
+            image = np.array(map(lambda letter: int(letter), line[6:])).reshape((16,8))
+            letters.append(image)
         f.close()
+        
+    
