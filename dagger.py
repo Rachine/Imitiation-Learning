8d1160538dd878a5743f6d03c7bb90bf0090f320
rachine <riadrachid3@gmail.com>
2017-01-09 19:13:35 +0100

    Dagger algorithm

--- dagger.py
+++ dagger.py
@@ -36,6 +36,7 @@ class DAgger(object):
         
     def process_ocr(self):
         """Process ocr dataset"""
+        print('Process ocr dataset')
         letters = []
         sequence = []
         f = open(self.ocr_path, 'r')
@@ -61,6 +62,7 @@ class DAgger(object):
         
     def build_initial_dataset(self):
         """Build Initial dataset of trajectories to mimic"""
+        print('Build dataset and labels')
         self.process_ocr()
         num_words = len(self.words)
         for idx in range(num_words): 
@@ -70,25 +72,55 @@ class DAgger(object):
                 self.dataset[self.words_fold[idx]].append(X.tolist())
                 self.labels[self.words_fold[idx]].append(self.sequences[idx][jdx])        
         
-    def aggregate_dataset(self, agg_data = [], hat_policy, test_fold = 9):
+    def aggregate_dataset(self, hat_policy, test_fold = 9, agg_data = []):
         """Aggregate original trajectories dataset with new generated from policy"""
+        print('Aggregate dataset')
         num_words = len(self.words)
         for idx in range(num_words):
             if self.words_fold[idx] != test_fold: 
                 for jdx in range(1,len(self.words[idx])):
                     X = np.concatenate([self.words[idx][jdx], np.array([self.sequences[idx][jdx-1]])],  axis = 0)
                     y_pred = hat_policy.predict(X)
-                    if y_pred != self.sequences[idx][jdx] and jdx != (len(self.words[idx]-1)):
+                    if y_pred != self.sequences[idx][jdx] and jdx != (len(self.words[idx])-1):
                         X_new = np.concatenate([self.words[idx][jdx+1], y_pred],  axis = 0)
-                        agg_data.append([X_new,self.sequences[idx][jdx+1]])
+                        agg_data[0].append(X_new.tolist())
+                        agg_data[1].append(self.sequences[idx][jdx+1])
         return agg_data
                     
-    def run(self, X,y):
+    def run(self, N = 10):
         """Fit the policy classifier trained on a given dataset"""
-        clf = linear_model.SGDClassifier()
-        clf.fit(X,y)
-        return clf
+        print('full run')
+        self.build_initial_dataset()        
+        hat_policies = []
         
-    
+        test_fold = 9
+        X_train = sum([self.dataset[i] for i in self.dataset.keys() if i != test_fold], [])
+        y_train = sum([self.labels[i] for i in self.labels.keys() if i != test_fold], [])
+        agg_data = [X_train,y_train]
+        
+        hat_policy = svm.LinearSVC(loss="hinge")
+        hat_policy.fit(agg_data[0],agg_data[1])
+        hat_policies.append(hat_policy)
+        
+        hamming_scores = []
+        y_pred = hat_policy.predict(self.dataset[test_fold])
+        hamming_scores.append(hamming_loss(y_pred,self.labels[test_fold]))
+        
+        for iter in range(1,N):
+            print(iter)
+            agg_data = self.aggregate_dataset(hat_policy,test_fold,agg_data)
+            hat_policy = svm.LinearSVC(loss="hinge")
+            hat_policy.fit(agg_data[0],agg_data[1])
+            hat_policies.append(hat_policy)
+            y_pred = hat_policy.predict(self.dataset[test_fold])
+            hamming_scores.append(hamming_loss(y_pred,self.labels[test_fold]))
+            
+        plt.plot(hamming_scores)
+        plt.show()
+        return hamming_scores
+        
+if __name__ == "__main__":
+    dag = DAgger()
+    scores = dag.run()
         
         
