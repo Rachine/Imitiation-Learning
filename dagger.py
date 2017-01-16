9839a1b7c3d763993c945a24365942c7b9ba514d
Kimia <kimianadjahi@gmail.com>
2017-01-16 10:05:27 +0100

    modifs on seen_states

--- dagger.py
+++ dagger.py
@@ -43,7 +43,7 @@ class DAgger(object):
         self.sequences = []
         self.dataset = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
         self.labels = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[]}
-        self.seen_states = []
+        self.seen_states = set()
         
     def process_ocr(self):
         """Process ocr dataset"""
@@ -77,39 +77,37 @@ class DAgger(object):
         self.process_ocr()
         num_words = len(self.words)
         for idx in range(num_words): 
-            for jdx in range(1,len(self.words[idx])): 
-                X = np.zeros((128,26))
-                X[:,self.sequences[idx][jdx-1]] = self.words[idx][jdx]
-                X = X.reshape((1,3328))
+            for jdx in range(1, len(self.words[idx])):
+                X = np.zeros((128, 26))
+                X[:, self.sequences[idx][jdx-1]] = self.words[idx][jdx]
+                X = X.reshape((1, 3328))
                 X = sparse.coo_matrix(X)
                 self.dataset[self.words_fold[idx]].append(X)
                 self.labels[self.words_fold[idx]].append(self.sequences[idx][jdx])
-                self.seen_states.append((idx,jdx,self.sequences[idx][jdx-1]))
+                self.seen_states.add((idx, jdx, self.sequences[idx][jdx-1]))
         
     def aggregate_dataset(self, hat_policy, agg_data, test_fold = 9):
         """Aggregate original trajectories dataset with new generated from policy"""
         print('Aggregate dataset')
         num_words = len(self.words)
         for idx in range(num_words):
-                if self.words_fold[idx] in [1]: 
-                    for jdx in range(1,len(self.words[idx])):
-                        X = np.zeros((128,26))
-                        X[:,self.sequences[idx][jdx-1]] = self.words[idx][jdx]
-                        X = X.reshape((1,3328))
+            if self.words_fold[idx] != test_fold:
+                    for jdx in range(1, len(self.words[idx])):
+                        X = np.zeros((128, 26))
+                        X[:, self.sequences[idx][jdx-1]] = self.words[idx][jdx]
+                        X = X.reshape((1, 3328))
                         X = sparse.coo_matrix(X)
                         y_pred = hat_policy.predict(X)
-    #                    if y_pred != self.sequences[idx][jdx] and jdx != (len(self.words[idx])-1):
-                        if jdx != (len(self.words[idx])-1):
-                            X_new = np.zeros((128,26))                        
-                            X_new[:,y_pred[0]] = self.words[idx][jdx+1]
-                            X_new = X_new.reshape((1,3328))
+                        # if y_pred != self.sequences[idx][jdx] and jdx != (len(self.words[idx])-1):
+                        if jdx != (len(self.words[idx])-1) and (idx, jdx+1, y_pred[0]) not in self.seen_states:
+                            X_new = np.zeros((128, 26))
+                            X_new[:, y_pred[0]] = self.words[idx][jdx+1]
+                            X_new = X_new.reshape((1, 3328))
                             X_new = sparse.coo_matrix(X_new)
-                            for el in agg_data[0]:
-                                if (idx,jdx,y_pred[0]) not in self.seen_states:
-                                    agg_data[0].append(X_new)
-                                    agg_data[1].append(self.sequences[idx][jdx+1])
-                                    self.seen_states.append((idx,jdx,y_pred[0]))
-                            
+                            agg_data[0].append(X_new)
+                            agg_data[1].append(self.sequences[idx][jdx+1])
+                            self.seen_states.add((idx, jdx, y_pred[0]))
+
         print("Dataset size after aggregation")
         print(len(agg_data[0]))
         return agg_data
@@ -121,12 +119,13 @@ class DAgger(object):
         final_scores = np.zeros(N)
         test_fold = 9
         hat_policies = []
-        X_train = sum([self.dataset[i] for i in self.dataset.keys() if i in [1]], [])
-        y_train = sum([self.labels[i] for i in self.labels.keys() if i in [1]], [])
-        agg_data = [X_train,y_train]
+        X_train = sum([self.dataset[i] for i in self.dataset.keys() if i != test_fold], [])
+        y_train = sum([self.labels[i] for i in self.labels.keys() if i != test_fold], [])
+        agg_data = [X_train, y_train]
         print("Dataset size at the beginning")
         print(len(agg_data[0]))
-        hat_policy = svm.SVC(C = 10,kernel='linear')
+        #hat_policy = svm.LinearSVC()
+        hat_policy = svm.SVC(C=10, kernel='linear')
 #        hat_policy = linear_model.SGDClassifier(loss="squared_hinge", penalty="l2", n_iter=10)
         hat_policy.fit(sparse.vstack(agg_data[0]),np.vstack(agg_data[1]))
         hat_policies.append(hat_policy)
@@ -136,10 +135,10 @@ class DAgger(object):
         print(1-hamming_loss(y_pred,self.labels[test_fold]))
         hamming_scores.append(hamming_loss(y_pred,self.labels[test_fold]))
         
-        for iter in range(1,N):
+        for iter in range(1, N):
             print(iter)
             agg_data = self.aggregate_dataset(hat_policy, agg_data, test_fold)
-            hat_policy = svm.SVC(C = 10,kernel='linear')
+            hat_policy = svm.SVC(C=10, kernel='linear')
 #            hat_policy = linear_model.SGDClassifier(loss="squared_hinge", penalty="l2", n_iter=10)
 
             hat_policy.fit(sparse.vstack(agg_data[0]),agg_data[1])
